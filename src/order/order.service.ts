7bcac3ed78dab64601a9afed1cd679791eaaa7dc
Olimova <ozoda4662@gmail.com>
2025-05-28 15:49:29 +0500

    order created

---
+++ src/order/order.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { InjectModel } from '@nestjs/sequelize';
+import { Order } from './models/order.entity';
+
+@Injectable()
+export class OrderService {
+  constructor(
+    @InjectModel(Order) private model:typeof Order
+  ){}
+
+  async create(createOrderDto: CreateOrderDto) {
+    const order=await this.model.create({...createOrderDto})
+    return order
+  }
+
+  findAll() {
+    return `This action returns all order`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} order`;
+  }
+
+  update(id: number, updateOrderDto: UpdateOrderDto) {
+    return `This action updates a #${id} order`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} order`;
+  }
+}
