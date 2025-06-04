1c371a643e2f621ab69b2b42db9bb7d0a23a9555
Olimova <ozoda4662@gmail.com>
2025-06-04 18:43:07 +0500

    file upload

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@nestjs/common";
-import { existsSync, mkdirSync, writeFile } from "fs";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { existsSync, mkdirSync, unlink, writeFile } from "fs";
 import { extname, join, resolve } from "path";
 import config from "src/config";
 import { catchError } from "src/utils/catch-error";
@@ -10,7 +10,7 @@ export class FileService{
     private readonly baseUrl = config.BASE_URL;
     private readonly filePath=resolve(__dirname,'..','..','..','uploads')
 
-    async createFile(file:Express.Multer.File){
+    async createFile(file:Express.Multer.File):Promise<string>{
         try {
             const ext=extname(file.originalname)
             const fileName=`${file.originalname.split('.')[0]}_${v4()}${ext}`
@@ -30,7 +30,34 @@ export class FileService{
     }
 
     async deleteFile(fileName:string):Promise<void>{
-        
+        try {
+            fileName=fileName.split(config.BASE_URL)[1]
+            const file=resolve(this.filePath,fileName)
+            if(!existsSync(file)){
+                throw new BadRequestException(`File does not exist:${fileName}`)
+            }
+            await new Promise<void>((res,rej)=>{
+                unlink(file,(err)=>{
+                    if(err) rej(err)
+                        res()
+                })
+            })
+        } catch (error) {
+            return catchError(error)
+        }
     }
 
+    async existFile(fileName:string) {
+        try {
+            fileName=fileName.split(config.BASE_URL)[1]
+            const file=resolve(this.filePath,fileName)
+            if(existsSync(file)){
+                return true
+            }else{
+                return false
+            }
+        } catch (error) {
+            catchError(error)
+        }
+    }
 }
